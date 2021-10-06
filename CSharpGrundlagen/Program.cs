90fc1f8995d4fb35d4964822d4f32227f63d6b3e
Michael Lindner <michael.lindner@mth-it-service.com>
2021-10-06 10:22:48 +0200

    Erstes Projekt Start

---
+++ CSharpGrundlagen/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace MeinProgramm
+{
+    #region "Meine erste Klasse"
+    class Program
+    {
+        static void Main(string[] param)
+        {
+#if DEBUG
+            System.Console.WriteLine("Hello World! Debug");
+#else
+        System.Console.WriteLine("Hello World! Release");
+#endif
+        }
+    }
+
+#endregion
+}
