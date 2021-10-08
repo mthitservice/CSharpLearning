2c76fb03b9e575145a7dc3bb127f18b6c709887b
Michael Lindner <michael.lindner@mth-it-service.com>
2021-10-08 11:16:47 +0200

    SQL Einfach fertig

--- SQLFensterAnwendung/MainWindow.xaml.cs
+++ SQLFensterAnwendung/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,10 +22,51 @@ namespace SQLFensterAnwendung
     /// </summary>
     public partial class MainWindow : Window
     {
+        string Server;
+        string Benutzer;
+        string Passwort;
         public MainWindow()
         {
             InitializeComponent();
-          
+            Server = ConfigurationManager.AppSettings.Get("SQLServer");
+            Benutzer = ConfigurationManager.AppSettings.Get("SQLServerBenutzer");
+            Passwort = ConfigurationManager.AppSettings.Get("SQLServerPasswort");
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            // SQL Verindung
+            // Öffnen der Verbindung
+            SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+            builder.DataSource = Server;
+            builder.UserID = Benutzer;
+            builder.Password = Passwort;
+            builder.InitialCatalog = "dbCanon";
+            txt_Connection.Text = builder.ConnectionString;
+            using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
+            {
+
+                // Senden einer Anweisung (Command)
+                using (SqlCommand command = new SqlCommand(txt_SQL.Text, connection))
+                {
+                    connection.Open();
+                    using (SqlDataReader reader= command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            txt_Result.Text += txt_Result.Text += reader.GetInt32(0).ToString()+ " ; " +reader.GetString(1);
+
+                        }
+
+                    }
+
+
+
+                }
+
+            }
+            // Schließen der Verbindung
+
         }
     }
 }
